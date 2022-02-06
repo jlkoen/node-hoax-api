8adab9d7562c1d9d96cc485d169a5f2a335d9629
John Koen <jkoen@smartronix.com>
2022-02-05 21:57:23 -0500

    Added basic user validation testing

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,6 +4,15 @@ const router = express.Router();
 const UserService = require('./UserService');
 
 router.post('/api/v1/users', async (req, res) => {
+  const user = req.body;
+  if (user.username === null) {
+    return res.status(400).send({
+      validationErrors: {
+        username: 'Username cannot be null',
+      },
+    });
+  }
+
   await UserService.save(req.body);
 
   return res.send({ message: 'User created' });
