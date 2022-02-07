0a88da9b1f485c423678fc660e3d65186392c10f
John Koen <jkoen@smartronix.com>
2022-02-07 10:43:12 -0500

    Added initial username & email validation

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -81,4 +81,14 @@ describe('User Registration', () => {
     const body = response.body;
     expect(body.validationErrors.username).toBe('Username cannot be null');
   });
+
+  it('returns E-mail cannot be null when email is null', async () => {
+    const response = await postUser({
+      username: 'user1',
+      email: null,
+      password: 'P4ssword',
+    });
+    const body = response.body;
+    expect(body.validationErrors.email).toBe('E-mail cannot be null');
+  });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,19 +3,39 @@ const User = require('./User');
 const router = express.Router();
 const UserService = require('./UserService');
 
-router.post('/api/v1/users', async (req, res) => {
+const validateUsername = (req, res, next) => {
   const user = req.body;
   if (user.username === null) {
-    return res.status(400).send({
-      validationErrors: {
-        username: 'Username cannot be null',
-      },
-    });
+    req.validationErrors = {
+      username: 'Username cannot be null',
+    };
   }
+  next();
+};
 
-  await UserService.save(req.body);
+const validateEmail = (req, res, next) => {
+  const user = req.body;
+  if (user.email === null) {
+    req.validationErrors = {
+      email: 'E-mail cannot be null',
+    };
+  }
+  next();
+};
 
-  return res.send({ message: 'User created' });
-});
+router.post(
+  '/api/v1/users',
+  validateUsername,
+  validateEmail,
+  async (req, res) => {
+    if (req.validationErrors) {
+      const response = { validationErrors: { ...req.validationErrors } };
+      return res.status(400).send(response);
+    }
+    await UserService.save(req.body);
+
+    return res.send({ message: 'User created' });
+  }
+);
 
 module.exports = router;
