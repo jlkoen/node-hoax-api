249c440cb8a8ea773ec889c8028cb3947c81b0b1
John Koen <jkoen@smartronix.com>
2022-02-16 10:01:30 -0500

    Added users get endpoint

---
+++ __tests__/UserListing.spec.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../src/server');
+
+describe('Listing Users', () => {
+  it('returns 200 ok when no users in database', async () => {
+    const response = await request(app).get('/api/1.0/users');
+    expect(response.status).toBe(200);
+  });
+
+  it('returns page object as response body', async () => {
+    const response = await request(app).get('/api/1.0/users');
+    expect(response.body).toEqual({
+      content: [],
+      page: 0,
+      size: 10,
+      totalPages: 0,
+    });
+  });
+});
