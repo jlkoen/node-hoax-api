f9c365c6503378fda3e21719f778b8be7d9a0c90
John Koen <jkoen@smartronix.com>
2022-02-27 15:25:42 -0500

    Added token expiration

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -5,7 +5,6 @@ const { check, validationResult } = require('express-validator');
 const ValidationException = require('../error/ValidationException');
 const pagination = require('../middleware/pagination');
 const ForbiddenException = require('../error/ForbiddenException');
-const tokenAuthentication = require('../middleware/tokenAuthentication');
 
 router.post(
   '/api/1.0/users',
@@ -63,17 +62,12 @@ router.post('/api/1.0/users/token/:token', async (req, res, next) => {
   }
 });
 
-router.get(
-  '/api/1.0/users',
-  pagination,
-  tokenAuthentication,
-  async (req, res) => {
-    const authenticatedUser = req.authenticatedUser;
-    const { page, size } = req.pagination;
-    const users = await UserService.getUsers(page, size, authenticatedUser);
-    res.send(users);
-  }
-);
+router.get('/api/1.0/users', pagination, async (req, res) => {
+  const authenticatedUser = req.authenticatedUser;
+  const { page, size } = req.pagination;
+  const users = await UserService.getUsers(page, size, authenticatedUser);
+  res.send(users);
+});
 
 router.get('/api/1.0/users/:id', async (req, res, next) => {
   try {
@@ -84,36 +78,28 @@ router.get('/api/1.0/users/:id', async (req, res, next) => {
   }
 });
 
-router.put(
-  '/api/1.0/users/:id',
-  tokenAuthentication,
-  async (req, res, next) => {
-    const authenticatedUser = req.authenticatedUser;
+router.put('/api/1.0/users/:id', async (req, res, next) => {
+  const authenticatedUser = req.authenticatedUser;
 
-    if (!authenticatedUser || authenticatedUser.id != req.params.id) {
-      return next(
-        new ForbiddenException('You are not authorized to update user')
-      );
-    }
-    await UserService.updateUser(req.params.id, req.body);
-    return res.send();
+  if (!authenticatedUser || authenticatedUser.id != req.params.id) {
+    return next(
+      new ForbiddenException('You are not authorized to update user')
+    );
   }
-);
+  await UserService.updateUser(req.params.id, req.body);
+  return res.send();
+});
 
-router.delete(
-  '/api/1.0/users/:id',
-  tokenAuthentication,
-  async (req, res, next) => {
-    const authenticatedUser = req.authenticatedUser;
-    // eslint-disable-next-line eqeqeq
-    if (!authenticatedUser || authenticatedUser.id != req.params.id) {
-      return next(
-        new ForbiddenException('You are not authorized to delete the user')
-      );
-    }
-    await UserService.deleteUser(req.params.id);
-    res.send();
+router.delete('/api/1.0/users/:id', async (req, res, next) => {
+  const authenticatedUser = req.authenticatedUser;
+  // eslint-disable-next-line eqeqeq
+  if (!authenticatedUser || authenticatedUser.id != req.params.id) {
+    return next(
+      new ForbiddenException('You are not authorized to delete the user')
+    );
   }
-);
+  await UserService.deleteUser(req.params.id);
+  res.send();
+});
 
 module.exports = router;
