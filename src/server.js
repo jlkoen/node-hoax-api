1e94ed3b9f2ef2c238b9b5e29b85f24f3045d2aa
John Koen <jkoen@smartronix.com>
2022-03-08 16:05:25 -0500

    Added image caching

--- src/server.js
+++ src/server.js
@@ -4,11 +4,23 @@ const errorHandler = require('./error/ErrorHandler');
 const AuthenticationRouter = require('./auth/AuthenticationRouter');
 const tokenAuthentication = require('./middleware/tokenAuthentication');
 const FileService = require('./file/FileService');
+const config = require('config');
+const path = require('path');
+
+const { uploadDir, profileDir } = config;
+const profileFolder = path.join('.', uploadDir, profileDir);
+
+const ONE_YEAR_IN_MILLIS = 365 * 24 * 60 * 60 * 1000;
 
 FileService.createFolders();
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  '/images',
+  express.static(profileFolder, { maxAge: ONE_YEAR_IN_MILLIS })
+);
 
 app.use(tokenAuthentication);
 
