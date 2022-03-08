6cb317fc3da068f8a747527a82840c40b92a2a99
John Koen <jkoen@smartronix.com>
2022-03-08 16:24:10 -0500

    Update user profile image

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -81,6 +81,9 @@ const getUser = async (id) => {
 const updateUser = async (id, updatedBody) => {
   const user = await User.findOne({ where: { id: id } });
   user.username = updatedBody.username;
+  if (user.image) {
+    await FileService.deleteProfileImage(user.image);
+  }
   if (updatedBody.image) {
     user.image = await FileService.saveProfileImage(updatedBody.image);
   }
