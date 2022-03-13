b1cd7303fe53ca070d3332092d698d9c57cdba5c
John Koen <jkoen@smartronix.com>
2022-03-13 18:25:19 -0400

    Added basic submit hoax functionality

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const tokenAuthentication = require('./middleware/tokenAuthentication');
 const FileService = require('./file/FileService');
 const config = require('config');
 const path = require('path');
+const HoaxRouter = require('./hoax/HoaxRouter');
 
 const { uploadDir, profileDir } = config;
 const profileFolder = path.join('.', uploadDir, profileDir);
@@ -26,6 +27,7 @@ app.use(tokenAuthentication);
 
 app.use(UserRouter);
 app.use(AuthenticationRouter);
+app.use(HoaxRouter);
 
 app.use(errorHandler);
 
